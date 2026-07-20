3bc9c04bc34e45aad1df486fff6d2c634505be63
Harshad <harshalvg19@gmail.com>
2026-07-20 15:03:54 -0500

    Create Footer.jsx

---
+++ Website/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+function Footer() {
+  return (
+    <footer
+      style={{
+        padding: "1rem 1.5rem",
+        borderTop: "1px solid #c08989",
+        marginTop: "2rem",
+        fontSize: "0.9rem",
+        color: "#4989d7",
+        textAlign: "center",
+      }}
+    >
+      <p style={{ margin: 0 }}>
+        Built for the NLP Nes project. Deployed on Vercel.
+      </p>
+    </footer>
+  );
+}
+
+
+export default Footer;
