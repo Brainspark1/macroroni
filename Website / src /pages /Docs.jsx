a55f4cf7a307b19dbca9c108964dcda1f8f46916
Harshad <harshalvg19@gmail.com>
2026-07-20 15:17:45 -0500

    Create Docs.jsx
    Documentation for instructions

---
+++ Website / src /pages /Docs.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+
+function Docs() {
+  return (
+    <section>
+      <h2>Documentation</h2>
+      <p>
+        On this website, you will be able to find a multitude
+        of different pieces of documentation needed to aid us all
+        in our journeys of making games fit for the future of VUI.
+        Featuring a correction layer to pick up on users correcting
+        themselves in the middle of their phrase, along with a semantic
+        mapping layer allowing for users to be able to give different
+        synonyms of a term within their game, nes_voice ultimately allows
+        users to give more complex commands such as “Mario, go jump on that goomba,”
+        and for these to be executed with the appropriate calculations necessary in
+        a connected emulator set-up by the developper utilizing the package. 
+      </p>
+      <h3>Installation</h3>
+      <p>
+        
+      </p>
+      <h3>Usage</h3>
+      <p>
+        To install and get started with nes_voice, simply run in terminal:
+        pip install nes_voice
+      </p>
+    </section>
+  );
+}
+
+
+export default Docs;
